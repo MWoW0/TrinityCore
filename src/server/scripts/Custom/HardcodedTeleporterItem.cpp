1cebe8691b5f51670228172d0974ee346dc2b3b9
Quazye <jonas.kerwin.hansen@gmail.com>
2019-01-25 22:02:54 +0100

    Reverted to using ENUM for readability. Added debug logging

--- src/server/scripts/Custom/HardcodedTeleporterItem.cpp
+++ src/server/scripts/Custom/HardcodedTeleporterItem.cpp
@@ -2,6 +2,7 @@
 #include "ScriptedGossip.h"
 #include "ObjectGuid.h"
 #include "Item.h"
+#include "Log.h"
 
 enum GossipMenuOptions
 {
@@ -41,8 +42,8 @@ class HardcodedTeleporterItem : public ItemScript
 		// @reference ScriptedGossip.h#L87
 		// void TC_GAME_API AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action);
 		
-		AddGossipItemFor(player, 0, "|TInterface\\icons\\spell_arcane_teleportshattrath:20|tStarter Mall", GOSSIP_SENDER_MAIN, 200001);
-		AddGossipItemFor(player, 0, "|TInterface\\icons\\achievement_zone_tanaris_01:20|tCustom Dungeon", GOSSIP_SENDER_MAIN, 2000000);
+		AddGossipItemFor(player, 0, "|TInterface\\icons\\spell_arcane_teleportshattrath:20|tStarter Mall", GOSSIP_SENDER_MAIN, STARTER_MALL);
+		AddGossipItemFor(player, 0, "|TInterface\\icons\\achievement_zone_tanaris_01:20|tCustom Dungeon", GOSSIP_SENDER_MAIN, ZUL_FARAK);
 	}
 
 	/**
@@ -55,22 +56,26 @@ class HardcodedTeleporterItem : public ItemScript
 	    GossipMenu& gossipMenu = player->PlayerTalkClass->GetGossipMenu();
 
 	    if (menuId != gossipMenu.GetMenuId()) {
+            TC_LOG_INFO("scripts.Custom", "Tried to open gossip menu %u was expecting %u", gossipMenu.GetMenuId(), menuId);
 	        return;
 	    }
 
 	    GossipMenuItem const* item = gossipMenu.GetItem(gossipListId);
 	    if (!item) {
+            TC_LOG_INFO("scripts.Custom", "Could not find gossip item in list %u", gossipListId);
 	    	return;
 	    }
 
 	    uint32 gossipOptionId = item->OptionType;
 
+        TC_LOG_INFO("scripts.Custom", "Selected gossip ID %u", gossipOptionId);
+
 	    switch(gossipOptionId) {
-	    	case 200001://STARTER_MALL
+	    	case STARTER_MALL:
 	    		player->TeleportTo(530, -2278.14f, 5568.74f, 66.9998f, 5.9990100f);
 	    		break;
 
-	    	case 2000000://Zul Farrak
+	    	case ZUL_FARAK:
 	    		player->TeleportTo(1, -6806.85f, -2890.600f, 8.8868f, 6.257260f);
 	    		break;
 	    }
