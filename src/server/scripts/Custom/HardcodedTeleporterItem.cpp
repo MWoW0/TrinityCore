d3ebf6069cc341189fd5f1523ffaaf9ba30f5f61
doffykillzz <doffy.16@gmail.com>
2019-01-25 12:31:50 +0100

    Update HardcodedTeleporterItem.cpp

--- src/server/scripts/Custom/HardcodedTeleporterItem.cpp
+++ src/server/scripts/Custom/HardcodedTeleporterItem.cpp
@@ -41,8 +41,8 @@ class HardcodedTeleporterItem : public ItemScript
 		// @reference ScriptedGossip.h#L87
 		// void TC_GAME_API AddGossipItemFor(Player* player, uint32 icon, std::string const& text, uint32 sender, uint32 action);
 		
-		AddGossipItemFor(player, 0, "|TInterface\\icons\\spell_arcane_teleportshattrath:20|tStarter Mall", GOSSIP_SENDER_MAIN, 200001 STARTER_MALL);
-		AddGossipItemFor(player, 0, "|TInterface\\icons\\achievement_zone_tanaris_01:20|tCustom Dungeon", GOSSIP_SENDER_MAIN, ZUL_FARAK);
+		AddGossipItemFor(player, 0, "|TInterface\\icons\\spell_arcane_teleportshattrath:20|tStarter Mall", GOSSIP_SENDER_MAIN, 200001);
+		AddGossipItemFor(player, 0, "|TInterface\\icons\\achievement_zone_tanaris_01:20|tCustom Dungeon", GOSSIP_SENDER_MAIN, 2000000);
 	}
 
 	/**
@@ -66,11 +66,11 @@ class HardcodedTeleporterItem : public ItemScript
 	    uint32 gossipOptionId = item->OptionType;
 
 	    switch(gossipOptionId) {
-	    	case 200001: // STARTER_MALL:
+	    	case 200001://STARTER_MALL
 	    		player->TeleportTo(530, -2278.14f, 5568.74f, 66.9998f, 5.9990100f);
 	    		break;
 
-	    	case ZUL_FARAK:
+	    	case 2000000://Zul Farrak
 	    		player->TeleportTo(1, -6806.85f, -2890.600f, 8.8868f, 6.257260f);
 	    		break;
 	    }
